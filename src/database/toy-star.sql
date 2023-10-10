2a5023c3a1a4bfccbc1efb4a64160df52b392cdb
VishLucasHenrique <lucasproenca_11@hotmail.com>
2023-10-10 11:26:50 -0300

    Configurações e instalações das dependências

---
+++ src/database/toy-star.sql
@@ -0,0 +1,9 @@
+-- Active: 1688999163272@@127.0.0.1@3306
+CREATE TABLE users(
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    nickname TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL,
+    created_at TEXT DEFAULT(DATETIME()) NOT NULL
+);
